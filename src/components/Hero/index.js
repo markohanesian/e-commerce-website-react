9776665288fe9730cb7cfe2d273b8b6d7be66b4e
markohanesian <mso872@gmail.com>
2021-08-11 12:40:16 -0700

    Added sidebar functionality

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import Navbar from '../Navbar'
-import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroElements';
+import React, { useState } from "react";
+import Navbar from "../Navbar";
+import SideBar from "../SideBar";
+import {
+  HeroContainer,
+  HeroContent,
+  HeroItems,
+  HeroH1,
+  HeroP,
+  HeroBtn,
+} from "./HeroElements";
 
 export default function Hero() {
-    return (
-        <HeroContainer>
-            <Navbar />
-            <HeroContent>
-                <HeroItems>
-                    <HeroH1>Greatest Marketing</HeroH1>
-                    <HeroP>Expert marketing services</HeroP>
-                    <HeroBtn>Book a consultation</HeroBtn>
-                </HeroItems>
-            </HeroContent>
-        </HeroContainer>
-    )
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    };
+
+  return (
+    <HeroContainer>
+          <Navbar toggle={toggle} />
+      <SideBar isOpen={isOpen} toggle={toggle} />
+      <HeroContent> 
+        <HeroItems>
+          <HeroH1>Greatest Marketing</HeroH1>
+          <HeroP>Expert marketing services</HeroP>
+          <HeroBtn>Book a consultation</HeroBtn>
+        </HeroItems>
+      </HeroContent>
+    </HeroContainer>
+  );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Nav, NavLink, NavIcon, Bars } from './NavbarElements'
 
-export default function Navbar() {
+export default function Navbar({toggle}) {
     return (
         <>
             <Nav>
                 <NavLink to='/'>Marketing</NavLink>
-                <NavIcon>
+                <NavIcon onClick={toggle}>
                     <p>Menu</p>
                     <Bars />
                 </NavIcon>
