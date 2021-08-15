b02a45485775cd4c1dc40b08d03151b768e4e6a0
markohanesian <mso872@gmail.com>
2021-08-15 15:45:32 -0700

    Added data file for services

---
+++ src/components/Services/data.js
@@ -0,0 +1,27 @@
+import service1 from '../../images/branding.jpg'
+import service2 from '../../images/consulting.jpg'
+import service3 from '../../images/strategy.jpeg'
+
+export const servicesData = [
+    {
+        img: service1,
+        alt: 'branding',
+        name: 'branding',
+        desc: 'branding services for your business...',
+        button: 'GO'
+    },
+    {
+        img: service2,
+        alt: 'consulting',
+        name: 'consulting',
+        desc: 'consulting for business...',
+        button: 'GO'
+    },
+    {
+        img: service3,
+        alt: 'strategy',
+        name: 'strategy',
+        desc: 'strategy services for your business...',
+        button: 'GO'
+    }
+]
